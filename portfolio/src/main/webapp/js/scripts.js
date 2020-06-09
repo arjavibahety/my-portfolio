a0fda2c3680afaeda3e32e365361b2801ce194d4
arjavibahety <arjavibahety@gmail.com>
2020-06-10 00:04:39 +0800

    Add fetch to request DataServlet content

--- portfolio/src/main/webapp/js/scripts.js
+++ portfolio/src/main/webapp/js/scripts.js
@@ -4,24 +4,24 @@
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-resume/blob/master/LICENSE)
  */
 (function ($) {
-  "use strict"; // Start of use strict
+  'use strict'; // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $("html, body").animate(
+        $('html, body').animate(
           {
             scrollTop: target.offset().top,
           },
           1000,
-          "easeInOutExpo"
+          'easeInOutExpo'
         );
         return false;
       }
@@ -29,17 +29,24 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
   });
 
   // MDB Lightbox Init
   $(function () {
-    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+    $('#mdb-lightbox-ui').load('mdb-addons/mdb-lightbox-ui.html');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $("body").scrollspy({
-    target: "#sideNav",
+  $('body').scrollspy({
+    target: '#sideNav',
   });
 })(jQuery); // End of use strict
+
+async function getData() {
+    const response = await fetch('/data');
+    const responseText = await response.text();
+    console.log(responseText);
+    document.getElementById('greeting-container').innerHTML = responseText;
+}
