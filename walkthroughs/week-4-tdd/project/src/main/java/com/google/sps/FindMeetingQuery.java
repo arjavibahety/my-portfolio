b30973d40bc5e7ede533b82404cba48bcbe7aacf
arjavibahety <arjavibahety@gmail.com>
2020-06-24 13:07:43 +0800

    Add find meeting query algo

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,92 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return new ArrayList<>();
+    } 
+    
+    PriorityQueue<TimeRange> busyTimeRanges = getBusyTimeRanges(events, request);
+    ArrayList<TimeRange> combinedBusyTimeRanges = getCombinedBusyTimeRanges(busyTimeRanges);
+    Collection<TimeRange> result = getAvailableTimeRanges(combinedBusyTimeRanges, request);
+    return result;
+  }
+
+  private boolean eventHasRequestAttendees(Collection<String> requestAttendees, Event event) {
+    Set<String> eventAttendees = event.getAttendees();
+
+    for (String requestAttendee : requestAttendees) {
+      if (eventAttendees.contains(requestAttendee)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+  
+  private PriorityQueue<TimeRange> getBusyTimeRanges(Collection<Event> events, MeetingRequest request) {
+    PriorityQueue<TimeRange> busyTimeRanges = new PriorityQueue<>(TimeRange.ORDER_BY_START);
+    Collection<String> requestAttendees = request.getAttendees();
+
+    for (Event event : events) {
+      if (eventHasRequestAttendees(requestAttendees, event)) {
+        TimeRange eventTimeRange = event.getWhen();
+        busyTimeRanges.add(eventTimeRange); 
+      }
+    }
+
+    return busyTimeRanges;
+  }
+
+  private ArrayList<TimeRange> getCombinedBusyTimeRanges(PriorityQueue<TimeRange> busyTimeRanges) {
+    ArrayList<TimeRange> combinedBusyTimeRanges = new ArrayList<>();
+
+    while (!busyTimeRanges.isEmpty()) {
+      TimeRange busyTimeRange = busyTimeRanges.poll();
+      int startTime = busyTimeRange.start();
+      int endTime = busyTimeRange.end();
+      
+      while (!busyTimeRanges.isEmpty() && busyTimeRanges.peek().overlaps(busyTimeRange)) {
+        TimeRange nextOverlappingBusyTimeRange = busyTimeRanges.poll();
+        endTime = Math.max(endTime, nextOverlappingBusyTimeRange.end());
+      }
+      
+      TimeRange newTimeRange = TimeRange.fromStartEnd(startTime, endTime, false);
+      combinedBusyTimeRanges.add(newTimeRange);
+    }
+
+    return combinedBusyTimeRanges;
+  }
+
+  private void processNewTimeRange(ArrayList<TimeRange> availableTimeRanges, TimeRange newTimeRange, MeetingRequest request) {
+    if (newTimeRange.duration() >= request.getDuration()) {
+      availableTimeRanges.add(newTimeRange);
+    }
+  }
+
+  private Collection<TimeRange> getAvailableTimeRanges(ArrayList<TimeRange> combinedBusyTimeRanges, MeetingRequest request) {
+    ArrayList<TimeRange> availableTimeRanges = new ArrayList<>();
+    int endOfLastBusyTimeRange = TimeRange.START_OF_DAY;
+
+    if (combinedBusyTimeRanges.size() == 0) {
+      availableTimeRanges.add(TimeRange.WHOLE_DAY);
+      return availableTimeRanges;
+    }
+
+    for (TimeRange busyTimeRange : combinedBusyTimeRanges) {
+      int start = busyTimeRange.start();
+      TimeRange newTimeRange = TimeRange.fromStartEnd(endOfLastBusyTimeRange, start, false);
+      processNewTimeRange(availableTimeRanges, newTimeRange, request);
+      endOfLastBusyTimeRange = busyTimeRange.end();
+    }
+
+    TimeRange newTimeRange = TimeRange.fromStartEnd(endOfLastBusyTimeRange, TimeRange.END_OF_DAY, true);
+    processNewTimeRange(availableTimeRanges, newTimeRange, request);
+    return availableTimeRanges;
   }
 }
